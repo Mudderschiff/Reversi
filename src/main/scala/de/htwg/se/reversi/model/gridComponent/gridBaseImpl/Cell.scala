9a5b3d753153d9dcd78cb8bae2394f35a4c88863
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2018-12-19 13:17:30 +0100

    +- Cell toString +- CellSpac

--- src/main/scala/de/htwg/se/reversi/model/gridComponent/gridBaseImpl/Cell.scala
+++ src/main/scala/de/htwg/se/reversi/model/gridComponent/gridBaseImpl/Cell.scala
@@ -5,5 +5,5 @@ import de.htwg.se.reversi.model.gridComponent.CellInterface
 case class Cell(value: Int) extends CellInterface {
   def isSet: Boolean = value != 0
 
-  override def toString: String = if(value == 0) " " else if(value == 1) "S" else if (value == 2) "W" else " "
+  override def toString: String = value.toString.replace('0', ' ').replace('1', 'W').replace('2', 'B')
 }

f69c5b0bd8d8570233ff6ac8037eb9509955dea6
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2018-12-26 19:25:16 +0100

    +- additional GameStates

--- src/main/scala/de/htwg/se/reversi/controller/controllerComponent/GameStatus.scala
+++ src/main/scala/de/htwg/se/reversi/controller/controllerComponent/GameStatus.scala
@@ -2,14 +2,18 @@ package de.htwg.se.reversi.controller.controllerComponent
 
 object GameStatus extends Enumeration{
   type GameStatus = Value
-  val IDLE, SET, FINISH, NEW, OddNumber= Value
+  val IDLE, RESIZE, SET, NEW, LOADED, COULDNOTLOAD, SAVED, CANDIDATES, FINISHED = Value
 
   val map = Map[GameStatus, String](
     IDLE -> "",
     NEW -> "A new game was created",
     SET -> "A Cell was set",
-    FINISH -> "Game has finished",
-    OddNumber -> "Couldn't create field. The number must be eval.")
+    RESIZE -> "Game was resized",
+    CANDIDATES -> "Showing candidates",
+    FINISHED -> "Game successfully FINISHED",
+    LOADED ->"A new Game was loaded",
+    COULDNOTLOAD -> "The file could not be loaded",
+    SAVED ->  "The Game was saved")
 
   def message(gameStatus: GameStatus) = {
     map(gameStatus)

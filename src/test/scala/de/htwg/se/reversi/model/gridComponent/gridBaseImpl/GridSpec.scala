009acb81e9877b13c283d4e27b0eb294fcdb7e7b
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2019-01-15 19:49:29 +0100

    +- test highlight

--- src/test/scala/de/htwg/se/reversi/model/gridComponent/gridBaseImpl/GridSpec.scala
+++ src/test/scala/de/htwg/se/reversi/model/gridComponent/gridBaseImpl/GridSpec.scala
@@ -6,52 +6,6 @@ import org.scalatest.{Matchers, WordSpec}
 
 class GridSpec extends WordSpec with Matchers {
   "A Grid is the playingfield of Reversi. A Grid" when {
-    "function validTurns" should {
-      val freshGrid = new Grid(8).createNewGrid
-      var turns = freshGrid.getValidTurns(1)
-      "return for a fresh game these 4 possible turns for player1" in {
-        turns.head should be(Turn(3, 3, 5, 3, Direction.Down))
-        turns(1) should be(Turn(3, 3, 3, 5, Direction.Right))
-        turns(2) should be(Turn(4, 4, 2, 4, Direction.Up))
-        turns(3) should be(Turn(4, 4, 4, 2, Direction.Left))
-      }
-      "and these 4 turns for player2" in {
-        turns = freshGrid.getValidTurns(2)
-        turns.head should be(Turn(3, 4, 5, 4, Direction.Down))
-        turns(1) should be(Turn(3, 4, 3, 2, Direction.Left))
-        turns(2) should be(Turn(4, 3, 2, 3, Direction.Up))
-        turns(3) should be(Turn(4, 3, 4, 5, Direction.Right))
-      }
-      "and no turns for anything else" in {
-        freshGrid.getValidTurns(0).size should be(0)
-        freshGrid.getValidTurns(3).size should be(0)
-      }
-      "test scenarios for DownLeft, UpRight, UpLeft, DownRight" in {
-        var grid = Grid(new Matrix[Cell](Vector(Vector(Cell(0), Cell(1), Cell(2)), Vector(Cell(1), Cell(1), Cell(1)), Vector(Cell(0), Cell(0), Cell(0)))))
-        grid.getValidTurns(2)(2) should be(Turn(0,2,2,0,Direction.DownLeft))
-
-        grid = Grid(new Matrix[Cell](Vector(Vector(Cell(0), Cell(1), Cell(0)), Vector(Cell(1), Cell(1), Cell(0)), Vector(Cell(2), Cell(1), Cell(0)))))
-        grid.getValidTurns(2)(2) should be(Turn(2,0,0,2,Direction.UpRight))
-
-        grid = Grid(new Matrix[Cell](Vector(Vector(Cell(0), Cell(0), Cell(0)), Vector(Cell(1), Cell(1), Cell(1)), Vector(Cell(0), Cell(1), Cell(2)))))
-        grid.getValidTurns(2)(2) should be(Turn(2,2,0,0,Direction.UpLeft))
-
-        grid = Grid(new Matrix[Cell](Vector(Vector(Cell(2), Cell(1), Cell(0)), Vector(Cell(1), Cell(1), Cell(1)), Vector(Cell(0), Cell(0), Cell(0)))))
-        grid.getValidTurns(2)(2) should be(Turn(0,0,2,2,Direction.DownRight))
-      }
-    }
-    "to be constructed" should {
-      "be created with the length of its edges as size. Practically relevant are size 1, 3, and 8" in {
-        val tinygrid = new Grid(1)
-        val smallGrid = new Grid(3)
-        val normalGrid = new Grid(8)
-        val bigGrid = new Grid(16)
-      }
-      "for test purposes only created with a Matrix of Cells" in {
-        val bigGrid = Grid(new Matrix(2, Cell(0)))
-        val testGrid = Grid(Matrix[Cell](Vector(Vector(Cell(0), Cell(0)), Vector(Cell(0), Cell(0)))))
-      }
-    }
     "created properly but empty" should {
       val tinygrid = new Grid(1)
       val smallGrid = new Grid(3)
@@ -73,11 +27,15 @@ class GridSpec extends WordSpec with Matchers {
     "prefilled with values 1 to n" should {
       val tinyGrid = Grid(new Matrix[Cell](Vector(Vector(Cell(1)))))
       val smallGrid = Grid(new Matrix[Cell](Vector(Vector(Cell(1), Cell(2)), Vector(Cell(3), Cell(4)))))
+      //val empty = tinyGrid.reset()
       "have the right values in the right places" in {
         smallGrid.cell(0, 0) should be(Cell(1))
         smallGrid.cell(0, 1) should be(Cell(2))
         smallGrid.cell(1, 0) should be(Cell(3))
         smallGrid.cell(1, 1) should be(Cell(4))
+      }
+      "values should be resetable" in {
+        tinyGrid.reset(0,0).cell(0,0) should be(Cell(0))
       }
       "have Houses with the right Cells" in {
         tinyGrid.row(0).cell(0) should be(Cell(1))
@@ -92,6 +50,66 @@ class GridSpec extends WordSpec with Matchers {
         smallGrid.col(1).cell(0) should be(Cell(2))
         smallGrid.col(1).cell(1) should be(Cell(4))
       }
+    }
+    "to be constructed" should {
+      "be created with the length of its edges as size. Practically relevant are size 1, 3, and 8" in {
+        val tinygrid = new Grid(1)
+        val smallGrid = new Grid(3)
+        val normalGrid = new Grid(8)
+        val bigGrid = new Grid(16)
+      }
+      "for test purposes only created with a Matrix of Cells" in {
+        val bigGrid = Grid(new Matrix(2, Cell(0)))
+        val testGrid = Grid(Matrix[Cell](Vector(Vector(Cell(0), Cell(0)), Vector(Cell(0), Cell(0)))))
+      }
+    }
+    "In A Game" should {
+      val freshGrid = new Grid(8).createNewGrid
+      var turns = freshGrid.getValidTurns(1)
+      "return for a fresh game these 4 possible turns for player1" in {
+        turns.head should be(Turn(3, 3, 5, 3, Direction.Down))
+        turns(1) should be(Turn(3, 3, 3, 5, Direction.Right))
+        turns(2) should be(Turn(4, 4, 2, 4, Direction.Up))
+        turns(3) should be(Turn(4, 4, 4, 2, Direction.Left))
+      }
+      "and these 4 turns for player2" in {
+        turns = freshGrid.getValidTurns(2)
+        turns.head should be(Turn(3, 4, 5, 4, Direction.Down))
+        turns(1) should be(Turn(3, 4, 3, 2, Direction.Left))
+        turns(2) should be(Turn(4, 3, 2, 3, Direction.Up))
+        turns(3) should be(Turn(4, 3, 4, 5, Direction.Right))
+      }
+      "these turns are highlighted" in {
+        var highlighted = freshGrid.highlight(2)
+        print(highlighted)
+        highlighted.cell(2,3) should be(Cell(3))
+        highlighted.cell(3,2) should be(Cell(3))
+        highlighted.cell(4,5) should be(Cell(3))
+        highlighted.cell(5,4) should be(Cell(3))
+      }
+      "and no turns for anything else" in {
+        freshGrid.getValidTurns(0).size should be(0)
+        freshGrid.getValidTurns(3).size should be(0)
+      }
+      "these turns arent highlightet" in {
+
+      }
+      "test scenarios for DownLeft, UpRight, UpLeft, DownRight" in {
+        var grid = Grid(new Matrix[Cell](Vector(Vector(Cell(0), Cell(1), Cell(2)), Vector(Cell(1), Cell(1), Cell(1)), Vector(Cell(0), Cell(0), Cell(0)))))
+        grid.getValidTurns(2)(2) should be(Turn(0,2,2,0,Direction.DownLeft))
+
+        grid = Grid(new Matrix[Cell](Vector(Vector(Cell(0), Cell(1), Cell(0)), Vector(Cell(1), Cell(1), Cell(0)), Vector(Cell(2), Cell(1), Cell(0)))))
+        grid.getValidTurns(2)(2) should be(Turn(2,0,0,2,Direction.UpRight))
+
+        grid = Grid(new Matrix[Cell](Vector(Vector(Cell(0), Cell(0), Cell(0)), Vector(Cell(1), Cell(1), Cell(1)), Vector(Cell(0), Cell(1), Cell(2)))))
+        grid.getValidTurns(2)(2) should be(Turn(2,2,0,0,Direction.UpLeft))
+
+        grid = Grid(new Matrix[Cell](Vector(Vector(Cell(2), Cell(1), Cell(0)), Vector(Cell(1), Cell(1), Cell(1)), Vector(Cell(0), Cell(0), Cell(0)))))
+        grid.getValidTurns(2)(2) should be(Turn(0,0,2,2,Direction.DownRight))
+      }
+    }
+    "highlight " should {
+
     }
   }
   "A Game is finished" when {

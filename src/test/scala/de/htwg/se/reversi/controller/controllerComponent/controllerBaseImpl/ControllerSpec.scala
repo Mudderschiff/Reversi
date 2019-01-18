a0be8cb3feee1667624b9b2bbfab9ab8efe5918b
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2019-01-18 23:27:43 +0100

    +- Moves Tui test to controller for more coverage

--- src/test/scala/de/htwg/se/reversi/controller/controllerComponent/controllerBaseImpl/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/reversi/controller/controllerComponent/controllerBaseImpl/ControllerSpec.scala
@@ -1,6 +1,6 @@
 package de.htwg.se.reversi.controller.controllerComponent.controllerBaseImpl
 
-import de.htwg.se.reversi.model.gridComponent.gridBaseImpl.Grid
+import de.htwg.se.reversi.model.gridComponent.gridBaseImpl.{Cell, Grid, Matrix}
 import java.io.File
 
 import de.htwg.se.reversi.controller.controllerComponent.GameStatus
@@ -11,18 +11,48 @@ import org.scalatest.{Matchers, WordSpec}
 
 class ControllerSpec extends WordSpec with Matchers {
   "A Controller" when {
-    "empty" should {
-      val smallGrid = new Grid(4)
-      val controller = new Controller(smallGrid)
+    var controller = new Controller(new Grid(8))
+    "creating empty grid" should {
+      "be empty" in {
+        controller.createEmptyGrid()
+        controller.grid should be(new Grid(8))
+      }
       "initialise with a starting player" in {
-        controller.grid.cell(0, 0).isSet should be(false)
         controller.activePlayer should be(controller.getActivePlayer())
       }
-      "Botstate is disabled" in {
+      "have no Bot active" in {
         controller.botState() should be(false)
       }
-      "IDLE gamestatus" in {
+      "have a IDLE game status" in {
         controller.gameStatus should be(GameStatus.IDLE)
+      }
+    }
+    "creating new Grid" should {
+      val p1 = Grid(new Matrix[Cell](Vector(
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(0), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(0), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(3), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(1),Cell(2), Cell(3), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(3), Cell(2),Cell(1), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(3),Cell(0), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(0), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(0), Cell(0), Cell(0), Cell(0)))))
+      val p2 = Grid(new Matrix[Cell](Vector(
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(0), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(0), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(3),Cell(0), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(3), Cell(1),Cell(2), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(2),Cell(1), Cell(3), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(3), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(0), Cell(0), Cell(0), Cell(0)),
+        Vector(Cell(0), Cell(0), Cell(0), Cell(0),Cell(0), Cell(0), Cell(0), Cell(0)))))
+      "set gamepieces correctly" in {
+        controller.createNewGrid()
+        if (controller.activePlayer == 1) {
+          controller.grid should be(p1)
+        } else {
+          controller.grid should be(p2)
+        }
       }
     }
   }

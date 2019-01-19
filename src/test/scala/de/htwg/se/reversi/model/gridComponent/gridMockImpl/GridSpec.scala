a4d611ff3416a2980a1858e20b0e98935432a684
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2019-01-20 00:01:00 +0100

    +- MockImpl test

--- src/test/scala/de/htwg/se/reversi/model/gridComponent/gridMockImpl/GridSpec.scala
+++ src/test/scala/de/htwg/se/reversi/model/gridComponent/gridMockImpl/GridSpec.scala
@@ -5,44 +5,28 @@ import org.scalatest.{Matchers, WordSpec}
 class GridSpec extends WordSpec with Matchers {
   "A Mock Grid" when {
     val grid = new Grid(2)
-    "created not properly but empty" should {
-      "always be size of 1 anyways" in {
+    "created properly but empty" should {
+      "always be size of 1" in {
         grid.size should be (1)
       }
-
       "return a EmptyCell" in {
         grid.cell(0,0) should be(EmptyCell)
       }
-
-      "set does nothing" in {
-        grid.set(0,0,1) should be(grid)
+      "EmptyCell isn't set" in {
+        grid.cell(0,0).isSet should be(false)
       }
-
       "return no Turns" in {
         grid.getValidTurns(1) should be(List())
       }
-
-      "creates an empty grid" in {
-        grid.createNewGrid should be(grid)
-      }
-
-      "game is eval" in {
+      "Begins in a tie" in {
         grid.evaluateGame() should be(0)
       }
 
-      "highlights nothing" in {
-        grid.highlight(1) should be(grid)
-      }
-
-      "bot does nothing" in {
-        grid.makeNextTurnBot(1) should be(grid)
-      }
-
-      "is never finished" in {
+      "is not finished" in {
         grid.finish(1) should be(false)
       }
 
-      "score is always 0" in {
+      "scoreboard: 0-0" in {
         grid.score() should be((0,0))
       }
 

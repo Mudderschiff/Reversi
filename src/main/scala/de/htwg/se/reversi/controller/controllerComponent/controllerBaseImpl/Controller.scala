1f446f661de58f37a0362b66cbdd9e66149a910a
RustProfi <janetzkymarno@gmail.com>
2019-05-15 10:15:51 +0200

    get put in 1 route

--- src/main/scala/de/htwg/se/reversi/controller/controllerComponent/controllerBaseImpl/Controller.scala
+++ src/main/scala/de/htwg/se/reversi/controller/controllerComponent/controllerBaseImpl/Controller.scala
@@ -30,104 +30,101 @@ class Controller @Inject()(var grid: GridInterface) extends ControllerInterface 
   implicit val executionContext = system.dispatcher
 
 
+  val groute: Route =
+    get {
+      pathSingleSlash {
+        complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "<h1>Controller</h1>"))
+      } ~
+        path("controller") {
 
-  val groute: Route = get {
-    pathSingleSlash {
-      complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "<h1>Controller</h1>"))
-    }
-    path("controller") {
-      complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Controller</h1>"))
-      //gridtoHtml("xD")
-    } ~
-      path("controller" / "gridSize") {
-        gridtoHtml(gridSize.toString)
-      } ~
-      path("controller" / "createEmptyGrid") {
-        createEmptyGrid()
-        gridtoHtml(gridToString)
-      } ~
-      path("controller" / "save") {
-        save()
-        gridtoHtml("success")
-      } ~
-      path("controller" / "load") {
-        load()
-        gridtoHtml("success")
-      } ~
-      path("controller" / "score") {
-        score()
-        gridtoHtml(gridToString)
-      } ~
-      path("controller" / "gameStatus") {
-        gridtoHtml(gameStatus.toString)
-      } ~
-      path("controller" / "finish") {
-        finish()
-        gridtoHtml(gridToString)
-      } ~
-      path("controller" / "gridToString") {
-      gridtoHtml(gridToString)
-      } ~
-      path("controller" / "evaluateGame") {
-        gridtoHtml(evaluateGame().toString)
-      } ~
-      path("controller" / "botState") {
-        gridtoHtml(botState().toString)
-      } ~
-      path("controller" / "enableBot") {
-        enableBot()
-        gridtoHtml("success")
-      } ~
-      path("controller" / "disableBot") {
-        disableBot()
-        gridtoHtml("success")
-      } ~
-      path("controller" / "bot") {
-        bot()
-        gridtoHtml("success")
-      } ~
-      path("controller" / "statusText") {
-        gridtoHtml(statusText)
-      } ~
-      path("controller" / "undo") {
-        undo
-        gridtoHtml("success")
-      } ~
-      path("controller" / "redo") {
-        redo
-        gridtoHtml("success")
-      }
-  }
-
-  val proute: Route = put {
-    pathSingleSlash {
-      complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, "<h1>Controller</h1>"))
-    }
-    path("controller") {
-      gridtoHtml("xD")
-    } ~
-      path("controller" / "resize") {
-        parameter("newSize".as[Int]) { news =>
-          resize(news)
+          gridtoHtml("xD")
+        } ~
+        path("controller" / "gridSize") {
+          gridtoHtml(gridSize.toString)
+        } ~
+        path("controller" / "createEmptyGrid") {
+          createEmptyGrid()
           gridtoHtml(gridToString)
-        }
-
-      } ~
-      path("controller" / "set") {
-        parameter("row".as[Int], "col".as[Int], "value".as[Int]) { (row, col, value) =>
-          set(row, col, value)
+        } ~
+        path("controller" / "createNewGrid") {
+          createNewGrid()
           gridtoHtml(gridToString)
+        } ~
+        path("controller" / "save") {
+          save()
+          gridtoHtml("success")
+        } ~
+        path("controller" / "load") {
+          load()
+          gridtoHtml("success")
+        } ~
+        path("controller" / "score") {
+          score()
+          gridtoHtml(gridToString)
+        } ~
+        path("controller" / "gameStatus") {
+          gridtoHtml(gameStatus.toString)
+        } ~
+        path("controller" / "finish") {
+          finish()
+          gridtoHtml(gridToString)
+        } ~
+        path("controller" / "gridToString") {
+          gridtoHtml(gridToString)
+        } ~
+        path("controller" / "evaluateGame") {
+          gridtoHtml(evaluateGame().toString)
+        } ~
+        path("controller" / "botState") {
+          gridtoHtml(botState().toString)
+        } ~
+        path("controller" / "enableBot") {
+          enableBot()
+          gridtoHtml("success")
+        } ~
+        path("controller" / "disableBot") {
+          disableBot()
+          gridtoHtml("success")
+        } ~
+        path("controller" / "bot") {
+          bot()
+          gridtoHtml("success")
+        } ~
+        path("controller" / "statusText") {
+          gridtoHtml(statusText)
+        } ~
+        path("controller" / "undo") {
+          undo
+          gridtoHtml("success")
+        } ~
+        path("controller" / "redo") {
+          redo
+          gridtoHtml("success")
         }
-      } ~
-      path("controller" / "cell") {
-        parameter("row".as[Int], "col".as[Int]) { (row, col) =>
-          gridtoHtml(cell(row, col).toString)
-        }
+    } ~
+      put {
+        path("controller" / "resize") {
+          parameter("newSize".as[Int]) { news =>
+            resize(news)
+            gridtoHtml(gridToString)
+          }
+
+        } ~
+          path("controller" / "set") {
+            parameter("row".as[Int], "col".as[Int], "value".as[Int]) { (row, col, value) =>
+              set(row, col, value)
+              gridtoHtml(gridToString)
+            }
+          } ~
+          path("controller" / "cell") {
+            parameter("row".as[Int], "col".as[Int]) { (row, col) =>
+              gridtoHtml(cell(row, col).toString)
+            }
+          }
       }
-  }
 
   def gridtoHtml(xd: String): StandardRoute = {
-    complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Controller</h1>" + "<p>"+xd+"</p>"))
+    complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Controller</h1>" + "<pre>"+xd+"</pre>"))
   }
 
   val bindingFuture = Http().bindAndHandle(groute, "localhost", 8070)

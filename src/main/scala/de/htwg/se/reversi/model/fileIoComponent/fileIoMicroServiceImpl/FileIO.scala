b66e10f3563d883eb6c33f795c5e12f230fb843c
RustProfi <janetzkymarno@gmail.com>
2019-06-21 19:55:26 +0200

    new version try

--- src/main/scala/de/htwg/se/reversi/model/fileIoComponent/fileIoMicroServiceImpl/FileIO.scala
+++ src/main/scala/de/htwg/se/reversi/model/fileIoComponent/fileIoMicroServiceImpl/FileIO.scala
@@ -17,13 +17,16 @@ import org.apache.http.HttpHeaders
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.client.methods.HttpGet
-
+import org.apache.http.impl.client.HttpClientBuilder
+import java.io.InputStream
+import java.util.Scanner
+import org.apache.http.HttpStatus
 
 class FileIO extends FileIOInterface {
 
   override def load: Option[GridInterface] = {
     var gridOption: Option[GridInterface] = None
-    val source: String = getJson("http://localhost:8070/load_grid")//fromURL("http://localhost:8070/load_grid").mkString
+    val source: String = getJson("http://localhost:8070/load_grid").get//fromURL("http://localhost:8070/load_grid").mkString
     val json: JsValue = Json.parse(source)
     val size = (json \ "grid" \ "size").get.toString.toInt
     val injector = Guice.createInjector(new ReversiModule)
@@ -75,7 +78,7 @@ class FileIO extends FileIOInterface {
   }
 
   override def loadPlayer: Int = {
-    getJson("http://localhost:8070/load_player").toInt
+    getJson("http://localhost:8070/load_player").get.toInt
     //fromURL("http://localhost:8070/load_player").mkString.toInt
   }
 
@@ -103,18 +106,32 @@ class FileIO extends FileIOInterface {
     val result = client.execute(post, new BasicResponseHandler())
   }
 
-  def getJson(url: String): String = {
-    val httpClient = HttpClients.createDefault()
-    val httpResponse = httpClient.execute(new HttpGet(url))
-    val entity = httpResponse.getEntity
-    var content = ""
-    if (entity != null) {
-      val inputStream = entity.getContent
-      content = io.Source.fromInputStream(inputStream).getLines.mkString
-      inputStream.close
-    }
-    content
+  def getJson(url: String): Option[String] = {
+    val httpClient = HttpClientBuilder.create.build
+    val get = new HttpGet(url)
+    //get.addHeader("Accept", "application/json")
+    val response = httpClient.execute(get)
+
+    if (response.getStatusLine.getStatusCode == HttpStatus.SC_OK) {
+      var is: InputStream = null
+      var sc: Scanner = null
+
+      try {
+        val entity = response.getEntity
+        val buffer = new StringBuilder()
+        is = entity.getContent
+        sc = new Scanner(is)
+        while (sc.hasNext) {
+          buffer.append(sc.nextLine())
+        }
+        Some(buffer.toString())
+      } catch {
+        case ex: Exception =>
+          if (is != null) is.close()
+          if (sc != null) sc.close()
+          if (response != null) response.close()
+          None
+      }
+    } else None
   }
-
-
 }
